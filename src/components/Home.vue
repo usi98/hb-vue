7cb41c1d51c6032b7dcdb06d2ab647725c4eebec
ylt <k74qrlin@gmail.com>
2021-03-10 09:33:53 +0800

    简单完成书籍

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavMenu></NavMenu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavMenu from "@/components/common/NavMenu";
+export default {
+  name: "Home",
+  components: {NavMenu}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -4,7 +4,8 @@ import Router from 'vue-router'
 //组件模块
 import AppIndex from './components/home/AppIndex'
 import Login from './components/Login'
-
+import Home from './components/Home'
+import LibraryIndex from "@/components/library/LibraryIndex";
 
 Vue.use(Router)
 
@@ -15,10 +16,25 @@ export default new Router({
         path: '/login', name: 'Login', component: Login
       },
       {
-        path: '/index', name: 'AppIndex', component: AppIndex,
-        meta: {
-          requireAuth: true
-        }
+        path: '/home', name: 'Home', component: Home,
+        children: [
+          {
+            path: '/index',
+            name: 'AppIndex',
+            component: AppIndex,
+            meta: {
+              requireAuth: true
+            }
+          },
+          {
+            path: '/library',
+            name: 'Library',
+            component: LibraryIndex,
+            meta: {
+              requireAuth: true
+            }
+          }
+        ]
       }
     ]
 })
